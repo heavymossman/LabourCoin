948c61c3403e888f702a4be4c8d62d16924e3f0d
Michael O'Sullivan <michael@nizamimedia.com>
2017-10-10 01:13:02 +0100

    added proof of work

--- labour-coin.js
+++ labour-coin.js
@@ -35,16 +35,33 @@
 			this.timestamp = timestamp;
 			this.data = data;
 			this.previousHash = previousHash;
-
+			this.nonce = 0;
 			this.hash = this.calculateHash();
 
 		};
 
 		calculateHash(){
 
-			return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+			return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
 
 		}
+
+		mineBlock(difficulty) {
+			// PROOF OF WORK
+			//We need to prove that a lot of computer power has gone into making a block, this is called mining. BitCOin requies a block to start with a limited
+			// amount of zeros, since you can not inflience the output of a hash function, you have to try it over and over to match the right number of zeros
+			//This is called the difficulty, create one new block evey ten minutes
+			//As computers get faster we need to create the difficult
+			//A block must be complicated to create so that it takes ten minutes (i.e complicated computer equation), so blocks are not spam created. 
+			//As computers get faster we must make the difficulty or equation harder so it still takes ten minutes to create a new block
+			// This while loop will keep running until its matches the right amount ofg zeros to start the hash
+		    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+		        this.nonce++; //none value is a random number, 
+		        this.hash = this.calculateHash();
+		    }
+
+		    console.log("BLOCK MINED: " + this.hash);
+		    }
 	}
 
 	class BlockChain {
@@ -53,6 +70,7 @@
 			//creating an array of blocks on the chain, initialising with just the Genesis[Hardie] block in place
 
 			this.chain = [this.createHardieBlock()];
+			this.difficulty = 6; //settomg ot ,anually for now
 		};
 
 		//This is the starting block of the chain and must always be created manually and during the initial coin creation = Gensis block = Hardie
@@ -68,21 +86,59 @@
 		addBlock(newBlock){
 			//add new blocks on to the chain
 
-			newBlock.previousHash = this.getLatestBlock().hash; //checking the previous blocks hash
-			newBlock.hash = newBlock.calculateHash(); //make sure whenever there are any changes to the block, the hashes update
+			newBlock.previousHash = this.getLatestBlock().hash; //checking the previous blocks hash and sets a new hash
+			newBlock.mineBlock(this.difficulty);
+
 
 			//Now we can push this block onto the chain
 			//ADD IN SECURITY CHECKS HERE! LOL LEDGER ETC
 			this.chain.push(newBlock);
 
-		}
+		};
+
+		isChainValid() {
+	        for (let i = 1; i < this.chain.length; i++){
+	            const currentBlock = this.chain[i];
+	            const previousBlock = this.chain[i - 1];
+
+	            if (currentBlock.hash !== currentBlock.calculateHash()) {
+	                return false;
+	            }
+
+	            if (currentBlock.previousHash !== previousBlock.hash) {
+	                return false;
+	            }
+	        }
+
+	        return true;
+    	};
 	}
 
 	let labourCoin = new BlockChain();
 
+	console.log("IS LABOUR COIN INTERGRITY AND BLOCKCHAIN VALID? " + labourCoin.isChainValid());
+
+	console.log('Mining block 1...');
+
 	labourCoin.addBlock(new Block(1,"10/07/2017",{amount: 1}));
-	labourCoin.addBlock(new Block(2,"10/07/2017, Michael O'Sullivan",{amount: 2}));
+	
+	console.log('Mining block 2...');
+
 	labourCoin.addBlock(new Block(3,"10/07/2017, Michael O'Sullivan",{amount: 100}));
 
-	console.log(JSON.stringify(labourCoin, null, 4)) //dunno what null does, but 4 seperates it with four spaces
+	console.log('Mining block 3...');
+
+	labourCoin.addBlock(new Block(2,"10/07/2017, Michael O'Sullivan",{amount: 2}));
+
+	console.log("IS LABOUR COIN INTERGRITY AND BLOCKCHAIN VALID? " + labourCoin.isChainValid());
+
+
+	if(labourCoin.isChainValid() === true ){
+		console.log(JSON.stringify(labourCoin, null, 4)) //dunno what null does, but 4 seperates it with four spaces
+	}
+
+	
+
+
+	
 
